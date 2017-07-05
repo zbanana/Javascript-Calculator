e05602b687ef1978c3221bce935a7b9a60840e40
Roberto Notario <zanaskw@gmail.com>
2017-07-05 10:44:21 +0100

    add Calculator + functions
    create main.js for UI logic

---
+++ assets/js/calculator.js
@@ -0,0 +1,54 @@
+function Calculator() {
+	var result = 0;
+	var operator;
+
+	this.getOperator = function() {
+		return operator;
+	};
+
+	this.setOperator = function(op) {
+		operator = op;
+	};
+
+	this.sum = function(num) {
+		return result += num;
+	};
+
+	this.subtract = function(num) {
+		return result -= num;
+	};
+
+	this.divide = function(num) {		
+		return result /= num;
+	};
+
+	this.multiply = function(num) {
+		return result *= num
+	};
+
+	this.calculate = function(num) {
+		if (result === 0) {
+			return result = num;
+		};
+		switch(operator) {
+			case "*":
+				return this.multiply(num);
+				break;
+			case "/":
+				return this.divide(num);
+				break;
+			case "+":
+				return this.sum(num);
+				break;
+			case "-":
+				return this.subtract(num);
+		}
+	}
+
+	this.resetAll = function() {
+		result = 0;
+		operator = undefined;
+	};
+
+
+}
---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	var calc = new Calculator();
+});
